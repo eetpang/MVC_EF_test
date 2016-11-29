e342f6021b6a6ddc3fcf2acbdaf73bb093d2b05c
eetpang <eetpang@gmail.com>
2016-11-28 20:56:42 -0500

    1st

---
+++ MVC_EF_test/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_EF_test.Startup))]
+namespace MVC_EF_test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
